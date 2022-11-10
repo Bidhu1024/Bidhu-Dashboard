4feb70c84c2cf9c4a19fd047c2d2bdc970aadedc
BidhubhushanGahan <bidhu1024av@gmail.com>
2022-11-10 21:36:09 +0530

    22:57

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
+import { useEffect } from "react";
 import "./App.css";
-
+import {BrowserRouter, Routes,Route} from 'react-router-dom'
+import { FiSettings } from "react-icons/fi";
+import {TooltipComponent} from '@syncfusion/ej2-react-popups'
 function App() {
   return (
     <div >
